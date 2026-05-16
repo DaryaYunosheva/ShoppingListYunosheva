0721a4698e65bf267be5dc9bf1b99b70878697dd
VladParahin <parakhinva@hotmail.com>
2026-05-17 01:28:08 +0300

    update NewNoteScreen

--- app/src/main/java/com/example/shoppinglistyunosheva/new_note_screen/NewNoteScreen.kt
+++ app/src/main/java/com/example/shoppinglistyunosheva/new_note_screen/NewNoteScreen.kt
@@ -1,16 +1,115 @@
 package com.example.shoppinglistyunosheva.new_note_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.shoppinglistyunosheva.R
+import com.example.shoppinglistyunosheva.ui.theme.BlueLight
+import com.example.shoppinglistyunosheva.ui.theme.DarkText
+import com.example.shoppinglistyunosheva.ui.theme.GrayLight
+import com.example.shoppinglistyunosheva.ui.theme.LightText
 
+@Preview(showBackground = true)
 @Composable
 fun NewNoteScreen() {
-    Text(
-        text = "New note screen",
-        modifier = Modifier.fillMaxSize()
-            .wrapContentWidth().wrapContentHeight())
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = GrayLight)
+    ){
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            )
+        ) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    TextField(
+                        modifier = Modifier.weight(1f).padding(top = 7.dp),
+                        value = "",
+                        onValueChange = {
+
+                        },
+                        label = {
+                            Text(
+                                text = "Заголовок",
+                                fontSize = 14.sp,
+                                color = LightText
+                            )
+                        },
+                        colors = TextFieldDefaults.colors(
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = BlueLight,
+                            focusedContainerColor = Color.White,
+                            unfocusedContainerColor = Color.White
+                        ),
+                        singleLine = true,
+                        textStyle = TextStyle(
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = DarkText
+                        )
+                    )
+
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.save_icon),
+                            contentDescription = "save",
+                            tint = DarkText)
+                    }
+
+                }
+
+                TextField(
+                    value = "",
+                    onValueChange = {
+
+                    },
+                    label = {
+                        Text(text = "Описание...",
+                            fontSize = 14.sp)
+                    },
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.White
+                    ),
+                    textStyle = TextStyle(
+                        fontSize = 14.sp,
+                        color = DarkText
+                    )
+                )
+            }
+        }
+    }
+
 }
